ef2757235901332807ca3f894485f5ba53b0face
Daniel Evans <migetman9@gmail.com>
2017-10-24 14:57:04 -0700

    add public settings

--- portal/models.py
+++ portal/models.py
@@ -4,9 +4,11 @@ class PortalCategory(MainModel):
     name = Column(UnicodeText)
     key = Column(UnicodeText)
     items = relationship('PortalItem', backref='category')
+    public = Column(Boolean, default=True)
 
 class PortalItem(MainModel):
     name = Column(UnicodeText)
     link = Column(UnicodeText)
     key = Column(UnicodeText)
     category_id = Column(UUID, ForeignKey('portal_category.id'))
+    public = Column(Boolean, default=True)
